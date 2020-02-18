588a066b7de6c0686776be9b43cf60990d57ab47
hyunki <dhchung317@gmail.com>
2020-02-18 06:21:11 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Projects from "./pages/Projects"
 import Skills from "./pages/Skills"
 import Contact from "./pages/Contact"
 
-import {Route,Switch} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
@@ -14,15 +14,15 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={Home}/>  
-      <Route exact path="/projects" component={Projects}/>  
-      <Route exact path="/skills" component={Skills}/> 
-      <Route exact path="/contact" component={Contact}/>    
-      <Route component={Error}/>
-    </Switch>
-    <Footer />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/skills" component={Skills} />
+        <Route exact path="/contact" component={Contact} />
+        <Route component={Error} />
+      </Switch>
+      <Footer />
     </>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -8,4 +8,3 @@ export default function Title({title}) {
         </div>
     )
 }
-ß
