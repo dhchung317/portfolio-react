aee7e2dbc143f357bcbf8c8d1ded8f2b33b12078
hyunki <dhchung317@gmail.com>
2020-07-16 20:25:43 -0400

    broekn

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import "./App.css"
-
+// import { play, exit } from './timelines'
+import { Route, Switch } from 'react-router-dom'
+// import { TransitionGroup, Transition } from "react-transition-group"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import Skills from "./pages/Skills"
 import Contact from "./pages/Contact"
-
-import { Route, Switch } from 'react-router-dom'
-
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+
 
 
 function App() {
   return (
     <>
+    {/* <BrowserRouter> */}
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/skills" component={Skills} />
-        <Route exact path="/contact" component={Contact} />
-        <Route component={Error} />
-      </Switch>
+      <Route render={(location) => {
+        // const { pathname, key } = location
+        return (
+        //   <TransitionGroup component={null}>
+        //     <Transition
+        //       key={key}
+        //       appear={true}
+        //       onEnter={(node, appears) => play(pathname, node, appears)}
+        //       onExit={(node, appears) => exit(node, appears)}
+        //       timeout={{ enter: 750, exit: 0 }}
+        //     >
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/skills" component={Skills} />
+                <Route exact path="/contact" component={Contact} />
+                <Route component={Error} />
+              </Switch>
+          //   </Transition>
+          // </TransitionGroup>
+        )
+      }} />
       <Footer />
+      {/* </BrowserRouter> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from '../img/logo_small.png'
 import { NavLink } from 'react-router-dom'
 
@@ -11,20 +11,13 @@ export default function Navbar() {
 
                 </div>
                 <div className="nav">
+                                
                 <ul className="nav-links">
-                    <span className="underline"/>
-                    <li>
-                        <NavLink exact to="/" activeClassName="active-link">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/projects" activeClassName="active-link">Projects</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/skills" activeClassName="active-link">Skills</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact" activeClassName="active-link">Contact</NavLink>
-                    </li>
+                    <li className="one"><NavLink exact to="/" activeClassName="active-link">Home</NavLink></li>
+                    <li className="two"><NavLink to="/projects" activeClassName="active-link">Projects</NavLink></li>
+                    <li className="three"> <NavLink to="/skills" activeClassName="active-link">Skills</NavLink></li>
+                    <li className="four"><NavLink to="/contact" activeClassName="active-link">Contact</NavLink></li>
+                    <hr className="underline"/>
                 </ul>
                 </div>
 
