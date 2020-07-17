3d7db27444e5b0a4f3636db6197b75e22c1840b6
hyunki <dhchung317@gmail.com>
2020-07-16 23:35:53 -0400

    using npm, implemented greensock

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./App.css"
-// import { play, exit } from './timelines'
+import { play, exit } from './timelines'
+import { TransitionGroup, Transition } from "react-transition-group"
 import { Route, Switch } from 'react-router-dom'
-// import { TransitionGroup, Transition } from "react-transition-group"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import Skills from "./pages/Skills"
@@ -11,37 +11,22 @@ import Error from "./pages/Error"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
-
-
 function App() {
   return (
     <>
-    {/* <BrowserRouter> */}
       <Navbar />
-      <Route render={(location) => {
-        const { pathname, key } = location
+      <Route render={() => {
         return (
-        //   <TransitionGroup component={null}>
-        //     <Transition
-        //       key={key}
-        //       appear={true}
-        //       onEnter={(node, appears) => play(pathname, node, appears)}
-        //       onExit={(node, appears) => exit(node, appears)}
-        //       timeout={{ enter: 750, exit: 0 }}
-        //     >
-              <Switch location={location}>
+              <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/projects" component={Projects} />
                 <Route exact path="/skills" component={Skills} />
                 <Route exact path="/contact" component={Contact} />
                 <Route component={Error} />
               </Switch>
-          //   </Transition>
-          // </TransitionGroup>
         )
       }} />
       <Footer />
-      {/* </BrowserRouter> */}
     </>
   );
 }
--- src/timelines/index.js
+++ src/timelines/index.js
@@ -1,48 +1,77 @@
-// import { gsap } from 'gsap';
-// import { TimelineMax as Timeline, Power1 } from 'gsap';
-// gsap.registerPlugin(Timeline, Power1);
+import { gsap } from 'gsap';
+import { TimelineMax as Timeline, Power1 } from 'gsap';
+gsap.registerPlugin(Timeline, Power1);
 
-// const getDefaultTimeline = (node, delay) => {
-//   const timeline = new Timeline({ paused: true });
-//   const content = node.querySelector('.content');
-//   const contentInner = node.querySelector('.content--inner');
+const getDefaultTimeline = (node, delay) => {
+    const timeline = new Timeline({ paused: true });
+    // const content = node.querySelectorAll('div');
 
-//   timeline
-//     .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
-//     .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
-//     .from(contentInner, 0.15, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn });
+    // timeline
+    //     .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
+    //     .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
 
-//   return timeline;
-// }
+    return timeline;
+}
 
-// const getHomeTimeline = (node, delay) => {
-//   const timeline = new Timeline({ paused: true });
-//   const texts = node.querySelectorAll('h1 > div');
 
-//   timeline
-//     .from(node, 0, { display: 'none', autoAlpha: 0, delay })
-//     .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
+const getProjectTimeline = (node, delay) => {
+    const timeline = new Timeline({ paused: true });
+    const content = node.querySelector('.project-list-container');
 
-//   return timeline;
-// }
+    timeline
+        .from(node, 0.3, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
+        .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
+        // .from(contentInner, 0.15, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn });
 
-// export const play = (pathname, node, appears) => {
-//   const delay = appears ? 0 : 0.5;
-//   let timeline
+    return timeline;
+}
 
-//   if (pathname === '/')
-//     timeline = getHomeTimeline(node, delay);
-//   else
-//     timeline = getDefaultTimeline(node, delay);
+const getSkillsTimeline = (node, delay) => {
+    const timeline = new Timeline({ paused: true });
+    const content = node.querySelectorAll('div > article');
+    const texts = node.querySelector('sTile');
+    // const contentInner = node.querySelector('.skill-list-content');
 
-//   window
-//     .loadPromise
-//     .then(() => requestAnimationFrame(() => timeline.play()))
-// }
+    timeline
+        .from(node, 0, { display: 'none', autoAlpha: 0, delay })
+        // .from(content, 0.15, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
+        // .from(content, 0.15, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn });
+        .staggerFrom(content, 0.175, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.075);
 
-// export const exit = (node) => {
-//   const timeline = new Timeline({ paused: true });
+    return timeline;
+}
 
-//   timeline.to(node, 0.15, { autoAlpha: 0, ease: Power1.easeOut });
-//   timeline.play();
-// }
+const getHomeTimeline = (node, delay) => {
+    const timeline = new Timeline({ paused: true });
+    const texts = node.querySelectorAll('h1 > div');
+
+    timeline
+        .from(node, 0, { display: 'none', autoAlpha: 0, delay })
+        .staggerFrom(texts, 0.375, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
+
+    return timeline;
+}
+
+export const play = (pathname, node, appears) => {
+    const delay = appears ? 0 : 0.5;
+    let timeline
+
+    if (pathname === '/')
+        timeline = getHomeTimeline(node, delay);
+    else if (pathname === '/projects')
+        timeline = getProjectTimeline(node, delay);
+    else if (pathname === '/skills')
+        timeline = getSkillsTimeline(node, delay);
+    else
+        timeline = getDefaultTimeline(node, delay);
+
+    window
+        .loadPromise
+        .then(() => requestAnimationFrame(() => timeline.play()))
+}
+
+export const exit = (node) => {
+    const timeline = new Timeline({ paused: true });
+    timeline.to(node, 0.375, { autoAlpha: 0, ease: Power1.easeOut });
+    timeline.play();
+}
