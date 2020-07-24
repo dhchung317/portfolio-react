fe7f65660ad0fc39409b5dd869d4eed1ae6e12dd
hyunki <dhchung317@gmail.com>
2020-07-23 22:20:44 -0400

    contact item img color adjusment

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
 export default function ContactItem({ item }) {
     const { name, link, icon } = item
