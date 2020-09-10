7d8ce25ec28c5b326f7e805ac7edc0529c0fca59
hyunki <dhchung317@gmail.com>
2020-09-10 01:01:33 -0400

    toggling boolean for hamburger menu placeholder working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../img/logo_small.png'
 import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
+
+    const [open, setOpen] = useState(false)
+
+    function toggleOpen(){
+        setOpen(!open)
+        console.log(open)
+    }
+
     return (
         <nav className="navbar">
             <div className="nav-logo">
@@ -11,7 +19,6 @@ export default function Navbar() {
                 </NavLink>
             </div>
             <div className="nav">
-
                 <ul className="nav-links">
                     <li className="one"><NavLink exact to="/" activeClassName="active-link">Home</NavLink></li>
                     <li className="two"><NavLink to="/projects" activeClassName="active-link">Projects</NavLink></li>
@@ -20,8 +27,22 @@ export default function Navbar() {
                     <hr className="underline" />
                 </ul>
             </div>
+            <button
+                type="button"
+                className="nav-btn"
+                onClick={() => toggleOpen()}
+            >
+                <img src={logo} />
+            </button>
 
         </nav>
     )
 
 }
+
+/**TODO: toggle hamburger menu when screen is small
+ *  add menu icon and align right
+ *  add media query, show icon when under certain height
+ *  add css to the media query to change menu items to stack vertically, and under the navbar height, with a width of screen
+ *  add toggle logic for hamburger menu, which will toggle when the hamburger menu shows and is clicked.
+ **/
